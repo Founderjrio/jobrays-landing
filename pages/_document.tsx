da23cc027076baca3dfac97b73eb7a7d1bf7dce9
Raghav Deora <founder@jobrays.io>
2025-07-29 16:57:39 +0530

    Add Pinterest domain verification tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
+        {/* Pinterest site verification */}
+        <meta name="p:domain_verify" content="92f09b4d05996a51a7465cbd3dd17e0f"/>
       </Head>
       <body>
         <Main />
